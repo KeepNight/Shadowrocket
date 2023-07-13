c52fc1e78eb9acaf6a9f7d0f03b8ec67248da172
KeepNight <71436041+KeepNight@users.noreply.github.com>
2023-07-13 23:05:15 +0800

    Delete ip.js

--- ip.js
+++
@@ -1,4 +0,0 @@
-let headers = $request.headers;
-const randomIp = () => Array(4).fill(0).map((_, i) => Math.floor(Math.random() * 255) + (i === 0 ? 1 : 0)).join('.');
-headers['X-Forwarded-For'] = randomIp();
-$done({headers});
