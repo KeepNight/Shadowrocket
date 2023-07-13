4e1ae971af53eb0f110f4ee0639e54495b90679b
KeepNight <71436041+KeepNight@users.noreply.github.com>
2023-07-13 19:56:54 +0800

    Update ip.js

--- ip.js
+++ ip.js
@@ -1,3 +1,4 @@
 let headers = $request.headers;
-headers['X-Forwarded-For'] = '8.8.8.8';
+const randomIp = () => Array(4).fill(0).map((_, i) => Math.floor(Math.random() * 255) + (i === 0 ? 1 : 0)).join('.');
+headers['X-Forwarded-For'] = randomIp();
 $done({headers});
